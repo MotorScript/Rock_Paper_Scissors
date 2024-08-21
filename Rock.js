7cbc81822d1962cd08edadf1647afafad0ff183c
lespaul1guitar <heritagehomesteaders@gmail.com>
2024-08-20 21:21:48 -0400

    Allow users to hit cancel and the program will still run

--- Rock.js
+++ Rock.js
@@ -14,52 +14,59 @@ function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
-let getUserChoice = () => prompt("Rock, Paper, or Scissors?").toLowerCase();
+const getUserChoice = () => prompt("Rock, Paper, or Scissors?").toLowerCase();
 let userChoice = null;
-try {
-    userChoice = getUserChoice();
-}
-catch (err) {
-    console.log("caught")
-    let userChoice = "Rock";
-}
-let randInt = getRndInteger(1, 4);
-let compChoice = "";
-
-if (randInt === 1) {
-    compChoice = "rock";
-} else if (randInt === 2) {
-    compChoice = "paper";
-} else if (randInt === 3) {
-    compChoice = "sciccors";
-}
-
-
-console.log(compChoice);
-
 
 while (userChoice != "rock" && userChoice != "paper" && userChoice != "scissors") {
-    userChoice = getUserChoice();
+    try {
+        userChoice = getUserChoice();
+    }
+    catch (err) {
+        console.log("caught")
+        userChoice = "Rock";
+    }
 }
 
-let win = (
-    (userChoice == "rock" && compChoice == "scissors") ||
-    (userChoice == "paper" && compChoice == "rock") ||
-    (userChoice == "scissors" && compChoice == "paper")
-);
-
-let tie = (userChoice == compChoice);
-
-if (win) {
-    window.alert("YOU WIN!!!")
-    console.log("YOU WIN!!!");
-} else if (tie) {
-    window.alert("TIE!")
-    console.log("TIE!");
-} else {
-    window.alert("YOU LOSE!")
-    console.log("YOU LOSE!");
+const getCompChoice = () => {
+    const randInt = getRndInteger(1, 4);
+    let compChoice = "";
+    if (randInt === 1) {
+        compChoice = "rock";
+    } else if (randInt === 2) {
+        compChoice = "paper";
+    } else if (randInt === 3) {
+        compChoice = "scissors";
+    }
+    return compChoice;
 }
 
+const compChoice = getCompChoice();
+
+const playGame = (userChoice, compChoice) => {
+
+
+
+    const win = (
+        (userChoice == "rock" && compChoice == "scissors") ||
+        (userChoice == "paper" && compChoice == "rock") ||
+        (userChoice == "scissors" && compChoice == "paper")
+    );
+
+    const tie = (userChoice == compChoice);
+
+    if (win) {
+        window.alert(`YOU WIN!!! ${userChoice} beats ${compChoice}!`)
+        console.log("YOU WIN!!!");
+    } else if (tie) {
+        window.alert(`TIE! ${userChoice} ties with ${compChoice}!`)
+        console.log("TIE!");
+    } else {
+        window.alert(`YOU LOSE! ${compChoice} beats ${userChoice}!`)
+        console.log("YOU LOSE!");
+    }
+
+}
+
+playGame(userChoice, compChoice)
 // For my wife :)
 // window.alert("YOU WIN!!")
